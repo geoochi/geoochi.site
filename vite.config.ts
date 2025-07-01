76244739dc1edb4f2e0683dde5ad04dad2b7e012
Geoo Chi <geoochi@geoochi.com>
2025-07-01 20:34:00 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import Components from 'unplugin-vue-components/vite'
 import Pages from 'vite-plugin-pages'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { ViteSSGOptions } from 'vite-ssg'
+import generateSitemap from 'vite-ssg-sitemap'
+
+const ssgOptions: ViteSSGOptions = {
+  script: 'async',
+  formatting: 'minify',
+  onFinished() {
+    generateSitemap({ hostname: 'https://geoochi.site' })
+  },
+}
 
 export default defineConfig({
   resolve: {
@@ -11,6 +21,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  ssgOptions,
   plugins: [
     tailwindcss(),
     Vue({
